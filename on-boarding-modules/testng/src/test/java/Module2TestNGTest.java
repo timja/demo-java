45308aa9e9decf7f7fb3fd65ad4029f2f2243da5
Tim Jacomb <timjacomb1+github@gmail.com>
2021-03-05 09:26:42 +0000

    Add failing test

--- on-boarding-modules/testng/src/test/java/Module2TestNGTest.java
+++ on-boarding-modules/testng/src/test/java/Module2TestNGTest.java
@@ -62,6 +62,56 @@ public class Module2TestNGTest {
 
 
     }
+
+    @Test
+    public void shouldOpenSafari() throws MalformedURLException {
+        /** Here we set environment variables from your local machine, or IntelliJ run configuration,
+         *  and store these values in the variables below. Doing this is a best practice in terms of test execution
+         *  and security. If you're not sure how to use env variables, refer to this guide -
+         * https://wiki.saucelabs.com/display/DOCS/Best+Practice%3A+Use+Environment+Variables+for+Authentication+Credentials
+         * or check testng-README.md */
+
+        String sauceUserName = System.getenv("SAUCE_USERNAME");
+        String sauceAccessKey = System.getenv("SAUCE_ACCESS_KEY");
+        String sauceURL = "https://ondemand.eu-central-1.saucelabs.com/wd/hub";
+
+        /**
+         * In this section, we will configure our test to run on some specific
+         * browser/os combination in Sauce Labs.*/
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability("username", sauceUserName);
+        capabilities.setCapability("accessKey", sauceAccessKey);
+        capabilities.setCapability("browserName", "Safari");
+        capabilities.setCapability("platform", "macOS 10.13");
+        capabilities.setCapability("version", "11.1");
+        capabilities.setCapability("build", "Onboarding Sample App - Java-TestNG");
+        capabilities.setCapability("name", "2-user-site");
+
+        /**
+         * In this section, we set the Remote WebDriver to run on Sauce Labs, and pass the capabilities.
+         * Then we perform some actions on an application.
+         * For this script, enter in your application's URL in place of 'https://www.saucedemo.com'. */
+
+        /** If you're accessing the EU data center, use the following endpoint:.
+         * https://ondemand.eu-central-1.saucelabs.com/wd/hub
+         * */
+        driver = new RemoteWebDriver(new URL(sauceURL), capabilities);
+        driver.navigate().to("https://www.saucedemo.com");
+        Assert.assertFalse(true);
+
+//        try {
+//            System.out.println("Sleeping for 60  seconds");
+//            Thread.sleep(1000 * 60);
+//            System.out.println("Finished sleeping");
+//        } catch (InterruptedException e) {
+//            e.printStackTrace();
+//        }
+        driver.navigate().to("https://www.saucedemo.com");
+
+
+    }
+
+
     /**
      * Below we are performing 2 critical actions. Quitting the driver and passing
      * the test result to Sauce Labs user interface. */
